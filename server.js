9ede1cc7cde2ce6e26b01f98029484b25be75768
Neha-Sonare <neha1906sonare@gmail.com>
2020-06-22 22:03:29 +0530

    deploy-project

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const ngApp = express();
-ngApp.use(express.static('./dist/angular-forms-validation'));
-ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/social-login-page/index.html'));
-});
-ngApp.listen(process.env.PORT || 8080);
